610cfedc1dde86e43851a69cf67176db53c98049
Lee Goddard <leegee@gmail.com>
2021-02-08 14:29:14 +0100

    style

--- memer/src/App.vue
+++ memer/src/App.vue
@@ -3,18 +3,46 @@
     <nav id="nav">
       <div v-show="$store.state.image">
         <router-link to="/" class="icon">🖉</router-link>
+        <button
+          id="show-customize"
+          v-show="$store.state.image"
+          @click="showModal = true"
+        >
+          Customise
+        </button>
+
         <router-link to="/layouts" class="icon">⬓</router-link>
         <router-link to="/save" class="save icon">⭳</router-link>
       </div>
     </nav>
     <router-view />
   </main>
+
+  <aside v-show="$store.state.image && showModal">
+    <transition name="modal">
+      <div class="modal-mask">
+        <div class="modal-wrapper">
+          <div class="modal-container">
+            <Settings />
+            <button id="close-modal" @click="showModal = false">OK</button>
+          </div>
+        </div>
+      </div>
+    </transition>
+  </aside>
 </template>
 
 <script lang="ts">
-import { Vue } from "vue-class-component";
+import { Vue, Options } from "vue-class-component";
+import Settings from "@/components/Settings.vue";
 
+@Options({
+  components: {
+    Settings,
+  },
+})
 export default class App extends Vue {
+  showModal = false;
   mounted() {
     const computed = window.getComputedStyle(this.$refs.app as Element);
     this.$store.commit("setDimensions", {
@@ -46,6 +74,104 @@ export default class App extends Vue {
   height: 100%;
 }
 
+#nav {
+  background: var(--app-fg);
+  font-size: 2rem;
+  display: flex;
+  justify-content: center;
+  align-items: space-between;
+  margin-bottom: 1rem;
+}
+#nav a {
+  text-decoration: none;
+  color: var(--app-bg);
+  opacity: 0.4;
+}
+#nav a.router-link-exact-active {
+  font-weight: bold;
+  opacity: 1;
+}
+#nav .icon {
+  display: inline-block;
+  font-weight: bolder;
+  min-width: 1em;
+  min-height: 1em;
+  margin-right: 1rem;
+}
+.icon.save {
+  -webkit-transform: scale(2, 1);
+  -moz-transform: scale(2, 1);
+  -ms-transform: scale(2, 1);
+  -o-transform: scale(2, 1);
+  transform: scale(2, 1);
+  margin-left: 1rem;
+}
+
+#close-modal {
+  --size: 3rem;
+  position: absolute;
+  right: calc(-0.5 * var(--size));
+  top: calc(-0.5 * var(--size));
+  background: black;
+  color: white;
+  border: 1pt solid white;
+  border-radius: 50%;
+  min-width: var(--size);
+  min-height: var(--size);
+}
+
+.modal-mask {
+  position: fixed;
+  z-index: 9998;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  width: 100%;
+  width: 100vw;
+  height: 100%;
+  height: 100vh;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: table;
+  transition: opacity 0.3s ease;
+}
+
+.modal-wrapper {
+  display: table-cell;
+  vertical-align: middle;
+}
+
+.modal-container {
+  position: relative;
+  width: 300px;
+  margin: 0px auto;
+  padding: 20px 30px;
+  background-color: var(--app-fg);
+  color: var(--app-bg);
+  border-radius: 4pt;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
+  transition: all 0.3s ease;
+  font-family: Helvetica, Arial, sans-serif;
+}
+
+.modal-enter {
+  opacity: 0;
+}
+
+.modal-leave-active {
+  opacity: 0;
+}
+
+.modal-enter .modal-container,
+.modal-leave-active .modal-container {
+  -webkit-transform: scale(1.1);
+  transform: scale(1.1);
+}
+
+#show-customize {
+  margin: 1rem;
+}
+
 .courier {
   font-family: "Courier New", Courier, monospace;
 }
@@ -56,8 +182,8 @@ export default class App extends Vue {
   font-family: Impact, Haettenschweiler, "Arial Narrow Bold", sans-serif;
 }
 .system {
-  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen, Ubuntu,
-    Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
 }
 .comic {
   font-family: "Comic Sans MS", "Comic Sans MS", cursive;
@@ -100,38 +226,5 @@ export default class App extends Vue {
 }
 .bolder-weight {
   font-weight: bolder;
-}
-
-#nav {
-  background: var(--app-fg);
-  font-size: 2rem;
-  display: flex;
-  justify-content: center;
-  align-items: space-between;
-  margin-bottom: 1rem;
-}
-#nav a {
-  text-decoration: none;
-  color: var(--app-bg);
-  opacity: 0.4;
-}
-#nav a.router-link-exact-active {
-  font-weight: bold;
-  opacity: 1;
-}
-#nav .icon {
-  display: inline-block;
-  font-weight: bolder;
-  min-width: 1em;
-  min-height: 1em;
-  margin-right: 1rem;
-}
-.icon.save {
-  -webkit-transform: scale(2, 1);
-  -moz-transform: scale(2, 1);
-  -ms-transform: scale(2, 1);
-  -o-transform: scale(2, 1);
-  transform: scale(2, 1);
-  margin-left: 1rem;
 }
 </style>
--- memer/src/views/Home.vue
+++ memer/src/views/Home.vue
@@ -11,40 +11,17 @@
     <DropZone msg="Drop an image here" v-show="!$store.state.image" />
     <Text v-show="$store.state.image" />
   </div>
-
-  <button
-    id="show-customize"
-    v-show="$store.state.image"
-    @click="showModal = true"
-  >
-    Customise
-  </button>
-
-  <div v-show="$store.state.image && showModal">
-    <transition name="modal">
-      <div class="modal-mask">
-        <div class="modal-wrapper">
-          <div class="modal-container">
-            <Settings />
-            <button id="close-modal" @click="showModal = false">OK</button>
-          </div>
-        </div>
-      </div>
-    </transition>
-  </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
 import DropZone from "@/components/DropZone.vue";
 import Text from "@/components/Text.vue";
-import Settings from "@/components/Settings.vue";
 
 @Options({
   components: {
     DropZone,
     Text,
-    Settings,
   },
 })
 export default class Home extends Vue {
@@ -63,56 +40,5 @@ export default class Home extends Vue {
   background-position: center;
   background-repeat: no-repeat;
   border: 1pt solid var(--app-fg);
-}
-
-.modal-mask {
-  position: fixed;
-  z-index: 9998;
-  top: 0;
-  left: 0;
-  bottom: 0;
-  right: 0;
-  width: 100%;
-  width: 100vw;
-  height: 100%;
-  height: 100vh;
-  background-color: rgba(0, 0, 0, 0.5);
-  display: table;
-  transition: opacity 0.3s ease;
-}
-
-.modal-wrapper {
-  display: table-cell;
-  vertical-align: middle;
-}
-
-.modal-container {
-  width: 300px;
-  margin: 0px auto;
-  padding: 20px 30px;
-  background-color: var(--app-fg);
-  color: var(--app-bg);
-  border-radius: 2px;
-  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
-  transition: all 0.3s ease;
-  font-family: Helvetica, Arial, sans-serif;
-}
-
-.modal-enter {
-  opacity: 0;
-}
-
-.modal-leave-active {
-  opacity: 0;
-}
-
-.modal-enter .modal-container,
-.modal-leave-active .modal-container {
-  -webkit-transform: scale(1.1);
-  transform: scale(1.1);
-}
-
-#show-customize {
-  margin: 1rem;
 }
 </style>
