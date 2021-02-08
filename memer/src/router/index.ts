df189f9bf1ecf8c04360bd4a7f6afa613eb18c3f
Lee Goddard <leegee@gmail.com>
2021-02-08 13:00:30 +0100

    Nav guard for image

--- memer/src/router/index.ts
+++ memer/src/router/index.ts
@@ -1,6 +1,11 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
-import Home from "../views/Home.vue";
-import Save from "../views/Save.vue";
+import Home from "@/views/Home.vue";
+import Save from "@/views/Save.vue";
+import store from "@/store";
+
+const requireImage = (store: any, next: Function) => {
+  !store.state.image ? next({ name: "Home" }) : next();
+};
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -12,10 +17,16 @@ const routes: Array<RouteRecordRaw> = [
     path: "/save",
     name: "Save",
     component: Save,
+    beforeEnter(to, from, next) {
+      requireImage(store, next);
+    }
   },
   {
     path: "/layouts",
     name: "Layouts",
+    beforeEnter(to, from, next) {
+      requireImage(store, next);
+    },
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
