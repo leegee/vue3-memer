f5b0a7a948bfa92675d3c77bbecece17a3e72028
Lee Goddard <leegee@gmail.com>
2021-02-09 14:39:56 +0100

    failure of types

--- memer/src/shims-vue.d.ts
+++ memer/src/shims-vue.d.ts
@@ -4,3 +4,18 @@ declare module '*.vue' {
   const component: DefineComponent<{}, {}, any>
   export default component
 }
+
+interface ClipboardItem {
+  items: Array<ClipboardItem>;
+}
+
+declare var ClipboardItem: {
+  prototype: ClipboardItem;
+  new(objects: Record<string, Blob>): ClipboardItem;
+};
+
+interface Clipboard {
+  read?(): Promise<Array<ClipboardItem>>;
+
+  write?(items: Array<ClipboardItem>): Promise<void>;
+}
